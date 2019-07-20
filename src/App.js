bdec45c80a35c3075640ff0f16e0ec8089a98fd6
Cristina Llamas <cristinallamasdevelopment@gmail.com>
2019-07-20 18:48:21 +0100

    Initial commit, local server API, axios fetch and showing json data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  // Declare a new state variable, which we'll call "data".
+  const [data, setData] = useState({ projects: [],jobs:[],about:{} });
+
+  // Used "as" componentDidMount.
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios.get('http://localhost:8000/data')
+      .then(response => {
+      // console.log(response);
+      setData(response.data);
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+      
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+      <h2>About</h2>
+      <h3>{data.about.name}</h3>
+      <h4>{data.about.title}</h4>
+      <p>{data.about.bio}</p>
+      <a href="../{data.about.bio}">Download my CV</a>
+
+      <h2>Examples</h2>
+      <ul>
+        {data.jobs.map(item => (
+          <li key={item.id}>
+            <h3>{item.project}</h3>
+            <h4>{item.company}</h4>
+            <p>{item.description}</p>
+            <ul>
+              {item.technologies.map((item,index) => (
+                <li key={index}>
+                  <h5>{item}</h5>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Side Projects</h2>
+      <ul>
+        {data.projects.map(item => (
+          <li key={item.id}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+
+    </React.Fragment>
   );
 }
 
