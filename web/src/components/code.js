d45ac49c0c0669aa88fc9fbfb6ed07ced0097183
Cristina Llamas <cristinallamasdevelopment@gmail.com>
2020-04-24 11:57:03 +0100

    left code component behind

---
+++ web/src/components/code.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {atomDark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import styles from './code.module.css'
+
+function Code ({node}) {
+  if (!node.code) {
+    return undefined
+  }
+  return (
+    <div className={styles.container}>
+      <SyntaxHighlighter language={node.language || 'text'} wrapLines style={atomDark}>
+        {node.code}
+      </SyntaxHighlighter>
+    </div>
+  )
+}
+
+export default Code
